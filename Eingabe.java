fda4432f9d17aa0e73272f9dea6bbb2171f912c9
KocadagGoezde <55785093+KocadagGoezde@users.noreply.github.com>
2020-10-13 16:51:19 +0200

    Add files via upload

--- Eingabe.java
+++ Eingabe.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
 public class Eingabe {
+    public static void main(String[] args) {
+        int a,b;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Geben Sie die erste Zahl ein: ");
+        a = sc.nextInt();
+
+        System.out.println("Geben Sie die zweite Zahl ein: ");
+        b = sc.nextInt();
+    }
 }
