f3eb76b4cc98211aa27bfb06fceda99e8731ab23
KocadagGoezde <55785093+KocadagGoezde@users.noreply.github.com>
2020-10-13 17:55:42 +0200

    Version 5
    Kommentare wurden hinzugefügt.

--- Eingabe.java
+++ Eingabe.java
@@ -1,39 +1,43 @@
 import java.util.Scanner;
 import java.util.*;
+
 public class Eingabe {
+
     public static void main(String[] args) {
         int a,b,c;
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);    //Scanner für die Benutzereingabe
 
-        try
+        try     //Anfang vom Try Block
         {
             System.out.println("Berechnung mit Integer Werten");
             System.out.println("Geben Sie die erste Zahl ein: ");
-            a = sc.nextInt();
+            a = sc.nextInt();               //Einlesen des ersten Int Wertes
 
             System.out.println("Geben Sie die zweite Zahl ein: ");
-            b = sc.nextInt();
+            b = sc.nextInt();               //Einlesen des zweiten Int Wertes
 
             c = a + b;
-            System.out.println("Die Summe der Zahlen beträgt: "+c);
+            System.out.println("Die Summe der Zahlen beträgt: "+c); //Ausgabe der Summe
         }
-        catch (InputMismatchException e)
+        catch (InputMismatchException e) //Anfang vom Catch Block
         {
-            System.out.println("Sie haben einen ungültigen Wert eingegeben.");
+            System.out.println("Sie haben einen ungültigen Wert eingegeben."); //Ausgabe bei Error
         }
 
         System.out.println("Berechnung mit Double Werten");
         double aa,bb,cc;
-        Scanner scc = new Scanner(System.in);
+
+        Scanner scc = new Scanner(System.in);   //Zweiter Scanner für die Eingabe
 
         System.out.println("Geben Sie eine Kommazahl ein: ");
-        aa = scc.nextDouble();
+        aa = scc.nextDouble();                          //Einlesen des ersten Double Wertes
+
         System.out.println("Geben Sie die zweite Kommazahl ein: ");
-        bb = scc.nextDouble();
+        bb = scc.nextDouble();                          //Einlesen des zweiten Double Wertes
 
         cc = aa+bb;
-        System.out.println("Die Summe der zwei Kommazahlen beträgt: "+cc);
+        System.out.println("Die Summe der zwei Kommazahlen beträgt: "+cc);          //Ausgabe der Summe
 
     }
 }
