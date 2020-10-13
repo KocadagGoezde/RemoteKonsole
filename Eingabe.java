e963da91637290cdc8119d4aeebc5bd5a93fa4ad
KocadagGoezde <55785093+KocadagGoezde@users.noreply.github.com>
2020-10-13 17:49:49 +0200

    Version 4
    Die Berechnung der Summe mit Int und Double Werten

--- Eingabe.java
+++ Eingabe.java
@@ -6,20 +6,34 @@ public class Eingabe {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Geben Sie die erste Zahl ein: ");
-        a = sc.nextInt();
-
-        System.out.println("Geben Sie die zweite Zahl ein: ");
-        b = sc.nextInt();
-
         try
         {
-            c=a+b;
+            System.out.println("Berechnung mit Integer Werten");
+            System.out.println("Geben Sie die erste Zahl ein: ");
+            a = sc.nextInt();
+
+            System.out.println("Geben Sie die zweite Zahl ein: ");
+            b = sc.nextInt();
+
+            c = a + b;
             System.out.println("Die Summe der Zahlen beträgt: "+c);
         }
         catch (InputMismatchException e)
         {
             System.out.println("Sie haben einen ungültigen Wert eingegeben.");
         }
+
+        System.out.println("Berechnung mit Double Werten");
+        double aa,bb,cc;
+        Scanner scc = new Scanner(System.in);
+
+        System.out.println("Geben Sie eine Kommazahl ein: ");
+        aa = scc.nextDouble();
+        System.out.println("Geben Sie die zweite Kommazahl ein: ");
+        bb = scc.nextDouble();
+
+        cc = aa+bb;
+        System.out.println("Die Summe der zwei Kommazahlen beträgt: "+cc);
+
     }
 }
